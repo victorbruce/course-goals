3c7354579fbcbcfda48be7f3928c9dc407867caf
victorbruce <victorbruce82@gmail.com>
2019-12-14 23:42:39 +0000

    flexbox in react-native

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
 import React, {useState} from 'react';
-import { StyleSheet, Text, View , Button} from 'react-native';
+import { StyleSheet, Text, View , TextInput, Button} from 'react-native';
 
 export default function App() {
   const [textOutput, setTextOutput] = useState('Open up App.js to start working on your app!')
   return (
-    <View style={styles.container}>
-      <Text>{textOutput}</Text>
-      <Button title="Change text" onPress={() => setTextOutput('Text changed!')}/>
+    <View style={{padding: 40, flexDirection: 'row'}}>
+      <View style={{backgroundColor: 'red', color:'white', width: 100, height: 100, justifyContent: 'center', alignItems:'center'}}>
+        <Text>1</Text>
+      </View>
+
+      <View style={{backgroundColor: 'blue', color: 'white', width: 100, height: 100, justifyContent: 'center', alignItems:'center'}}>
+        <Text>2</Text>
+      </View>
+
+      <View style={{backgroundColor: 'green', color: 'white', width: 100, height: 100, justifyContent: 'center', alignItems:'center'}}>
+        <Text>3</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  
 });
