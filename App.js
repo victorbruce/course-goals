5cc30e2c14956270f61791f2a9bd0fd97484e121
victorbruce <victorbruce82@gmail.com>
2019-12-15 00:26:22 +0000

    Handle user input

--- App.js
+++ App.js
@@ -1,18 +1,45 @@
-import React, {useState} from 'react';
-import { StyleSheet, Text, View , TextInput, Button} from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
-  const [textOutput, setTextOutput] = useState('Open up App.js to start working on your app!')
+  const [enteredGoal, setEneteredGoal] = useState("");
+
+  const goalInputHandler = enteredText => {
+    setEneteredGoal(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    console.log("adding goal...", enteredGoal);
+  };
+
   return (
-    <View style={{padding: 40}}>
-      <View style={{flexDirection:"row", justifyContent:"center", alignItems: "center"}}>
-        <TextInput placeholder="Course Goal" style={{width: '80%', borderWidth: 1, borderColor: 'black', padding:10}} />
-        <Button title="Add" />
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Course Goal"
+          style={styles.input}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+        />
+        <Button title="Add" onPress={addGoalHandler} />
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  
+  screen: {
+    padding: 40
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  input: {
+    width: "80%",
+    borderWidth: 1,
+    borderColor: "black",
+    padding: 10
+  }
 });
